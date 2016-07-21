ece950ec4a2b5a3e310260c1342407a98ac585bb
Giovane Boaviagem <giovanebribeiro@gmail.com>
2016-07-21 16:55:46 -0300

    first commit

---
+++ Giovanebribeiro.Lab/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab.Threads.Basic
+{
+    class Alpha
+    {
+        // this method will be called when thread starts
+        public void Beta()
+        {
+            while (true)
+            {
+                Console.WriteLine("Alpha.Beta is running on this thread");
+            }
+        }
+    };
+
+    class Program
+    {
+        static void StartStopJoin()
+        {
+            Console.WriteLine("Thread Start/Stop/Join Sample");
+
+            Alpha oAlpha = new Alpha();
+
+            // create the thread but not start it
+            Thread oThread = new Thread(new ThreadStart(oAlpha.Beta));
+
+            // start the thread
+            oThread.Start();
+
+            while (!oThread.IsAlive) ; // wait until thread starts
+
+            Thread.Sleep(1000); // put the main thread to sleep, to allow oThread to do some work.
+
+            // request oThread to stop
+            oThread.Abort();
+
+            // Wait until oThread finishes. Join also has overloads
+            // that take a millisecond interval or a TimeSpan object.
+            oThread.Join();
+
+            Console.WriteLine();
+            Console.WriteLine("Alpha.Beta has finished");
+
+            try
+            {
+                Console.WriteLine("Try to restart the Alpha.Beta thread.");
+                oThread.Start();
+            }
+            catch (ThreadStateException)
+            {
+                Console.Write("ThreadStateException trying to restart Alpha.Beta. ");
+                Console.WriteLine("Expected since aborted threads cannot be restarted.");
+            }
+            finally
+            {
+                Thread.Sleep(5000); // sleep for 5 sec before ends.
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            StartStopJoin();
+        }
+    }
+}
---
+++ Lab.Threads.ProducerConsumer/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab.Threads.ProducerConsumer
+{
+    public class Cell
+    {
+        int cellContents; //cell contents
+        bool readerFlag = false; // state flag
+
+        public int readFromCell() // the 'consumer' method
+        {
+            lock (this) // enter synchronization block
+            {
+                if (!readerFlag)
+                {
+                    try
+                    {
+                        Monitor.Wait(this); //waits for Monitor.Pulse
+                    }
+                    catch (SynchronizationLockException e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                    catch(ThreadInterruptedException e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+                Console.WriteLine("Consume: {0}", cellContents);
+                readerFlag = false; // reset the state flag to say consuming is done.
+                Monitor.Pulse(this); // Pulse tells Cell.WriteToCell that Cell.readFromCell is done.
+            } // end sync block
+            return cellContents;
+        }
+
+        public void WriteToCell(int n) // the 'producer' method
+        {
+            lock (this) // Enter sync block
+            {
+                if (readerFlag) // wait until Cell.ReadFromCell is done consuming
+                {
+                    try
+                    {
+                        Monitor.Wait(this); // Wait for the Monitor.Pulse in ReadFromCell
+                    }
+                    catch (SynchronizationLockException e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                    catch(ThreadInterruptedException e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+                cellContents = n;
+                Console.WriteLine("Produce: {0}", cellContents);
+                readerFlag = true; // set the state flag to say that producing is done
+                Monitor.Pulse(this); // Pulse tells Cell.readFromCell that Cell.WriteToCell is done.
+            } // end sync block
+        }
+    };
+
+    public class CellCons
+    {
+        Cell cell; 
+        int quantity = 1;
+
+        public CellCons(Cell box, int request)
+        {
+            cell = box;
+            quantity = request;
+        }
+
+        public void ThreadRun()
+        {
+            int valReturned;
+            for(int looper = 1; looper<= quantity; looper++)
+            {
+                // consume the result
+                valReturned = cell.readFromCell();
+            }
+        }
+    };
+
+    public class CellProd
+    {
+        Cell cell;
+        int quantity = 1;
+
+        public CellProd(Cell box, int request)
+        {
+            cell = box;
+            quantity = request;
+        }
+
+        public void ThreadRun()
+        {
+            for (int looper = 1; looper <= quantity; looper++)
+            {
+                // consume the result
+                cell.WriteToCell(looper); //producing
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
