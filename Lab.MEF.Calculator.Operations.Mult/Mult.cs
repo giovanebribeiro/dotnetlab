26980290dbdd579189b071c45c18c2b06896d136
Giovane Boaviagem <giovanebribeiro@gmail.com>
2016-07-27 16:59:57 -0300

    Implementing the MEF tutorial

--- Lab.MEF.Calculator.Operations.Mult/Mult.cs
+++ Lab.MEF.Calculator.Operations.Mult/Mult.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Lab.MEF.Calculator.Operations.Mult
 {
-    public class Mult
+    [Export(typeof(IOperation))]
+    [ExportMetadata("Symbol", '*')]
+    public class Mult : IOperation
     {
+        public int Operate(int left, int right)
+        {
+            return left * right;
+        }
     }
 }
